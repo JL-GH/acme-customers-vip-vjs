484cc39c1c339b56520350784767c83d7f34fe8e
JL-GH <jlingh1993@gmail.com>
2019-10-01 02:56:18 -0400

    uploading changes

--- script.js
+++ script.js
@@ -9,41 +9,74 @@ let customersCopy = customers.map((element) => {
 })
 
 const listAll = document.querySelector("#listAll")
-const button = document.getElementById("createBtn")
+const createBtn = document.getElementById("createBtn")
+const name = document.querySelector("#nameLabel")
+const email = document.getElementById("nameEmail")
+const isVIP = document.getElementById("isVIP")
+const newRow = document.createElement("div")
+const destroyCreate = document.createElement("button")
+const destroyBtn = document.querySelector(".destroy")
+const errMsg = document.querySelector("#errorMsg")
+// const addText = destroyBtn.createTe
+
 
 const createList = (arr) => {
   arr.forEach((elem) => {
     const newRow = document.createElement("div")
-    const destroy = document.createElement("button")
+    const destroyCreate = document.createElement("button")
     newRow.classList.add("row")
-    destroy.classList.add("destroy")
+    destroyCreate.classList.add("destroy")
+    destroyCreate.textContent = "destroy"
     newRow.innerHTML = `${elem.name} (${elem.email})`
     listAll.appendChild(newRow)
-    listAll.appendChild(destroy)
+    listAll.appendChild(destroyCreate)
   })
 }
 
 const addToList = () => {
-  const name = document.querySelector("#nameLabel").value
-  const email = document.getElementById("nameEmail").value
-  const isVIP = document.getElementById("isVIP").checked
-  const newRow = document.createElement("div")
-  const destroy = document.createElement("button")
+  let nameVal = name.value
+  let emailVal = email.value
+  let checked = isVIP.checked
 
-
-  customersCopy.push({name: name, email: email, isVIP: isVIP})
+  customersCopy.push({name: nameVal, email: emailVal, isVIP: checked})
 
   newRow.classList.add("row")
-  destroy.classList.add("destroy")
-  newRow.innerHTML = `${name} (${email})`
+  destroyCreate.classList.add("destroy")
+  destroyCreate.textContent = "destroy"
+  newRow.innerHTML = `${nameVal} (${emailVal})`
   listAll.appendChild(newRow)
-  listAll.appendChild(destroy)
+  listAll.appendChild(destroyCreate)
 }
 
-button.addEventListener("click", (ev) => {
+
+const validation = () => {
+  if ((name.value != "" && email.value != "") && email.value.split("").includes("@")) {
+    createBtn.disabled = false
+  }
+  else {
+    createBtn.disabled = true
+  }
+}
+
+createBtn.addEventListener("click", (ev) => {
   ev.preventDefault()
   addToList()
   console.log('test')
 })
+
+email.addEventListener("keyup", (ev) => {
+  let emailVal = email.value.split("")
+  if (!emailVal.includes('@')) {
+    errMsg.innerHTML = "email must contain an @"
+  }
+  else if (emailVal.includes("@")) {
+    errMsg.innerHTML = ""
+  }
+
+  validation()
+})
+
+name.addEventListener("oninput", validation())
+
 
 createList(customers)
